46778965ac587e43e3ea0b6b697488308cc7c57a
Alex Motor <wwweshka@rambler.ru>
2019-03-04 21:06:50 +0300

    Initial Commit

---
+++ Predictions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Predictions
+//
+//  Created by Alex Motor on 3/4/19.
+//  Copyright © 2019 Alexandr Motarykin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
