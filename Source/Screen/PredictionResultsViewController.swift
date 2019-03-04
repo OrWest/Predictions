4a612f999a498e45e943d824da6afd3e3d5f0deb
Alex Motor <wwweshka@rambler.ru>
2019-03-04 21:24:46 +0300

    Updated project structure.

---
+++ Source/Screen/PredictionResultsViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class PredictionResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Source/Screen/PredictionsMakeViewController.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class PredictionsMakeViewController: UIViewController {
+
+}
+
