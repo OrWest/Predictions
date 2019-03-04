def84a0bd63b64471bfc2ad60b339164d3266e4b
Alex Motor <wwweshka@rambler.ru>
2019-03-04 21:25:02 +0300

    Added code injection.

--- Source/Application/AppDelegate.swift
+++ Source/Application/AppDelegate.swift
@@ -7,6 +7,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        // Code injection (http://injectionforxcode.com)
+        #if DEBUG
+        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection10.bundle")?.load()
+        #endif
+        
         return true
     }
 
